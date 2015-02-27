f55dbb98c5d8cee90efd84b2c97d292c0e4679d4
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-02-27 16:22:11 +0100

    add pbs script for epoxy_espp

---
+++ scripts/pbs_epoxy_espp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#PBS -l nodes=1:ppn=20,walltime=12:00:00
+#PBS -N epoxy_espp
+
+module load intel/2014a Boost/1.55.0-intel-2014a-Python-2.7.6 h5py/2.2.1-intel-2014a-Python-2.7.6
+
+cd $PBS_O_WORKDIR
+source $HOME/espressopp/ESPRC
+
+NPROC=$( cat $PBS_NODEFILE  |  wc  -l )
+make epoxy_espp PY="mpirun -n ${NPROC} -f ${PBS_NODEFILE} python" RUN=${ll} FUNC=${FUNC}
+
