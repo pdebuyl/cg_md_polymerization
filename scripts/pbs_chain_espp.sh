864e1ec874f55b740bab0b38e72b5158a872f634
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-02-27 21:41:38 +0100

    edit pbs settings and run variables

--- scripts/pbs_chain_espp.sh
+++ scripts/pbs_chain_espp.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -l
-#PBS -l nodes=1:ppn=20,walltime=24:00:00
-#PBS -N chain_espp_${ll}
+#PBS -l nodes=1:ppn=20,walltime=12:00:00
+#PBS -N chain_espp
 
 module load intel/2014a Boost/1.55.0-intel-2014a-Python-2.7.6 h5py/2.2.1-intel-2014a-Python-2.7.6
 
 cd $PBS_O_WORKDIR
 source $HOME/espressopp/ESPRC
 
-make chain_espp PY="mpirun -n 20 python" CHAIN_N=08000 SITES=0001 RUN=${ll} TH=010 CHAIN_STEPS_ESPP=20000 RATE=${RATE} SITES=${SITES}
+NPROC=$( cat $PBS_NODEFILE  |  wc  -l )
+make chain_espp PY="mpirun -n ${NPROC} -f ${PBS_NODEFILE} python" CHAIN_N=10000 RUN=${ll} TH=${TH} CHAIN_STEPS_ESPP=${STEPS} RATE=${RATE} SITES=${SITES}
 
