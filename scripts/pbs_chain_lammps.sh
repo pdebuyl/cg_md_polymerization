524deca97d54e44d54abc49719d2b3a841705df0
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-02-27 21:41:18 +0100

    edit pbs settings

--- scripts/pbs_chain_lammps.sh
+++ scripts/pbs_chain_lammps.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
-#PBS -l nodes=1:ppn=20,walltime=24:00:00
-#PBS -N chain_lammps_$${ll}
+#PBS -l nodes=1:ppn=20,walltime=8:00:00
+#PBS -N chain_lammps
 
 module load intel/2014a 
 
 cd $PBS_O_WORKDIR
 NPROC=$( cat $PBS_NODEFILE  |  wc  -l )
 
-make chain_growth LMP="mpirun -n ${NPROC} -f ${PBS_NODEFILE} ${HOME}/lammps/src/lmp_ompi_icc" CHAIN_N=08000 SITES=${SITES} RUN=${ll} TH=025 CHAIN_STEPS_LAMMPS=8000000 RATE=${RATE}
+make chain_lammps LMP="mpirun -n ${NPROC} -f ${PBS_NODEFILE} ${HOME}/lammps/src/lmp_ompi_icc" CHAIN_N=10000 SITES=${SITES} RUN=${ll} TH=${TH} CHAIN_STEPS_LAMMPS=${MEGA}000000 RATE=${RATE} LAMMPS_CHAIN_FILE=${LCF}
 
